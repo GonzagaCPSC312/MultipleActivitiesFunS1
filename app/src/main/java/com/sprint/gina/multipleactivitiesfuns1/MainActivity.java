1bc6e1b96f4e60680dc4d8cfdd9a93a893fc3969
Gina Sprint <sprint@gonzaga.edu>
2021-10-21 11:57:37 -0700

    initial

---
+++ app/src/main/java/com/sprint/gina/multipleactivitiesfuns1/MainActivity.java
@@ -0,0 +1,91 @@
+package com.sprint.gina.multipleactivitiesfuns1;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivityTag";
+    ActivityResultLauncher<Intent> launcher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG, "onActivityResult: ");
+                        // this callback executes when MainActivity returns from
+                        // starting an activity (e.g. SecondActivity) that was
+                        // started for a result
+                        // BRB
+                    }
+                });
+
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: ");
+                // intent: a message to the Android OS
+                // specifying our "intent" to start another activity
+                // if the intent looks good, Android will start the activity
+                // 2 types
+                // explicit intent: you know the name of the activity class
+                // you want to start
+                // implicit intent: you want to let the user choose an app
+                // that can respond to the action you want to perform
+                // example actions
+                // Intent.ACTION_VIEW
+                // Intent.ACTION_SEND
+                // Intent.ACTION_DIAL
+                // Intent.ACTION_WEB_SEARCH
+
+                // explicit intent example
+                // lets start our SecondActivity
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                // source, destination
+                // lets say, SecondActivity needs some data
+                // from the MainActivity
+                // we can send this data in as "extra" information in the intent
+                // using key-value store
+                String username = "spike";
+                int pin = 1234;
+                intent.putExtra("username", username);
+                intent.putExtra("pin", pin);
+                // lets say, SecondActivity computes a result that we want
+                // in MainActivity (so SecondActivity sends the result back)
+                // in the NEW WAY: we need an ActivityResultLauncher
+//                startActivity(intent);
+                launcher.launch(intent);
+            }
+        });
+        
+        Button viewButton = findViewById(R.id.viewButton);
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        
+        Button sendButton = findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/sprint/gina/multipleactivitiesfuns1/SecondActivity.java
@@ -0,0 +1,37 @@
+package com.sprint.gina.multipleactivitiesfuns1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // get the extra information out
+        Intent intent = getIntent();
+        if (intent != null) { // good practice
+            // grab the extra information using the keys (names)
+            String username = intent.getStringExtra("username");
+            // username will be null if "username" is not a valid key
+            int pin = intent.getIntExtra("pin", 0);
+            // pin will be 0 if "pin" is not a valid key
+            Toast.makeText(this, username + " " + pin, Toast.LENGTH_SHORT).show();
+        }
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SecondActivity.this.finish();
+            }
+        });
+    }
+}
